62a9bfe12c96264eac88926171c010ce57edd9e6
luca-masera <lucamasera1995@gmail.com>
2024-03-08 14:40:07 +0100

    chiamata api ai countries

--- football.py
+++ football.py
@@ -1,5 +1,6 @@
 
-import requests #vado a stampare la libreria con pip install requests
+import requests #vado a installare la libreria con pip install requests
+import json # importo json per visionario il dizionario in modo più chiaro
 
 API_KEY = 'a7a65c15b993bf511bbbc57ec6671322'
 
@@ -18,3 +19,24 @@ print(url)
 
 response = requests.request("GET", url, headers=headers)
 print(response.status_code)
+
+# restituisce un JSON
+print(response.text)
+
+# converto il file json in un dizionario Python
+d= response.json()
+print(type(d))
+
+# serializzo, memorizzo i dati in una stringa
+print(json.dumps(d, indent=4))
+
+#faccio la chiamata all.endpoin countries
+endpoint = "countries"
+
+url = f"{base_url}{endpoint}"
+print(url)
+
+#faccio a richiesta api
+
+countries = requests.request("get", url, headers=headers)
+print(countries.json())
